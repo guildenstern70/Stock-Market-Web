4455ebbeccb374f541c9f5f7fe824647b98ed43a
Alessio <alessiosaltarin@gmail.com>
2012-02-10 12:05:57 +0100

    00

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,11 @@
 class HomeController < ApplicationController
+    include HomeHelper
     
     def index
         # Delete all previous players
         logger.debug('Deleting all players...')
-        _deleteall
+        deleteall
         logger.debug('... done')
-    end
-
-    def _deleteall
-        logger.debug("Player-controller-deleteall")
-        @players = Player.find(:all)
-        if (@players.length > 0)
-            @players.each { |player|
-              logger.debug("Deleting player " << player.name)
-              player.destroy
-            }
-        end
-
     end
 
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,15 @@
 module HomeHelper
+    
+    # Deletes all players from the Players table
+    def deleteall
+        logger.debug("Player-controller-deleteall")
+        @players = Player.find(:all)
+        if (@players.length > 0)
+            @players.each { |player|
+              logger.debug("Deleting player " << player.name)
+              player.destroy
+            }
+        end
+    end
+    
 end
